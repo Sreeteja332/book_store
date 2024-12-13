fe28fb41669aae83f7158514058313a367227ab4
Sreeteja332 <sreeteja332@gmail.com>
2024-12-13 16:08:46 +0530

    7.4

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -10,7 +10,7 @@ class Book(models.Model):
     rating=models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
     author=models.CharField(null=True,max_length=100)
     is_bestselling=models.BooleanField(default=False)
-    slug=models.SlugField(default="",null=False,db_index=True)#harry-potter-1
+    slug=models.SlugField(default="",blank=True,editable=False,null=False,db_index=True)#harry-potter-1
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
